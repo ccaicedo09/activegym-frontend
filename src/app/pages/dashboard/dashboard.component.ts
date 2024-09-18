14be6521f7f3a7c78bec647537dc6416836460f4
unknown <u20231213568@usco.edu.co>
2024-09-18 11:03:16 -0500

    Upper nav ready and aside created

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import DashboardNavComponent from "../../components/dashboard-nav/dashboard-nav.component";
+import { DashboardUpperNavComponent } from "../../components/dashboard-upper-nav/dashboard-upper-nav.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [DashboardNavComponent, DashboardUpperNavComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
