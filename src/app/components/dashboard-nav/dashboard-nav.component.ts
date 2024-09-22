69c9ed6d4246c2cae7061abf4a1828f96a688422
unknown <u20231213568@usco.edu.co>
2024-09-22 16:56:06 -0500

    feat: links hover effect and active link background

--- src/app/components/dashboard-nav/dashboard-nav.component.ts
+++ src/app/components/dashboard-nav/dashboard-nav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './dashboard-nav.component.html',
   styleUrl: './dashboard-nav.component.css'
 })
