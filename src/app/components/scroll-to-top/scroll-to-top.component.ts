ee4468aa90ada426851a4e68bbb694f82cd38ea7
unknown <u20231213568@usco.edu.co>
2024-10-20 16:27:34 -0500

    feat: scroll to top button

---
+++ src/app/components/scroll-to-top/scroll-to-top.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-to-top',
+  standalone: true,
+  imports: [],
+  templateUrl: './scroll-to-top.component.html',
+  styleUrl: './scroll-to-top.component.css'
+})
+export class ScrollToTopComponent {
+  isVisible = false;
+
+  // Escucha el evento de scroll en la ventana
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const yOffset = window.pageYOffset || document.documentElement.scrollTop;
+    this.isVisible = yOffset > 200; // Mostrar botón si el scroll es mayor a 200px
+  }
+
+  // Función para desplazar hacia arriba
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+}
