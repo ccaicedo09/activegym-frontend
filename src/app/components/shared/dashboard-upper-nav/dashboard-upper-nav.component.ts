60f48207255ddb90743b966994d671db9f37a31f
unknown <u20231213568@usco.edu.co>
2024-11-05 20:02:12 -0500

    fix date validator for membership creation and add notifications component

--- src/app/components/shared/dashboard-upper-nav/dashboard-upper-nav.component.ts
+++ src/app/components/shared/dashboard-upper-nav/dashboard-upper-nav.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { LoginService } from '../../../services/auth/login.service';
 import { Router, RouterLink } from '@angular/router';
+import NotificationBellComponent from "./notification-bell/notification-bell.component";
 
 @Component({
   selector: 'app-dashboard-upper-nav',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NotificationBellComponent],
   templateUrl: './dashboard-upper-nav.component.html',
   styleUrl: './dashboard-upper-nav.component.css'
 })
--- src/app/validators/dateValidator.ts
+++ src/app/validators/dateValidator.ts
@@ -3,10 +3,13 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 // Validador personalizado para la fecha mínima
 export function dateValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-
     const inputDate = new Date(control.value);
     const today = new Date();
-    today.setDate(today.getDate() - 1);
+
+    inputDate.setHours(0, 0, 0, 0);
+    today.setHours(0, 0, 0, 0);
+    inputDate.setDate(inputDate.getDate() + 1);
+
     return inputDate >= today ? null : { minDate: true };
   };
 }
