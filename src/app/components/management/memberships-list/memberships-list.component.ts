40734b82ee2eb38520b3abd471f344c41ee7b1c9
unknown <u20231213568@usco.edu.co>
2024-11-13 23:00:04 -0500

    feat: membership filters

--- src/app/components/management/memberships-list/memberships-list.component.ts
+++ src/app/components/management/memberships-list/memberships-list.component.ts
@@ -4,16 +4,17 @@ import { ActivatedRoute, Router, RouterLink, RouterModule } from '@angular/route
 import { Membership } from '../../../models/memberships/memberships.interface';
 import { MembershipsService } from '../../../services/memberships/memberships.service';
 import MembershipFormComponent from "../membership-form/membership-form.component";
-import { NgModel } from '@angular/forms';
+import { FormBuilder, FormGroup, NgModel, ReactiveFormsModule } from '@angular/forms';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MembershipType } from '../../../models/memberships/membershiptype.interface';
 import { LoginService } from '../../../services/auth/login.service';
 import { PdfService } from '../../../services/util/pdf.service';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-memberships-list',
   standalone: true,
-  imports: [RouterLink, CommonModule, MembershipFormComponent, MatTooltipModule],
+  imports: [RouterLink, CommonModule, MembershipFormComponent, MatTooltipModule, ReactiveFormsModule],
   templateUrl: './memberships-list.component.html',
   styleUrl: './memberships-list.component.css'
 })
@@ -24,6 +25,7 @@ export default class MembershipsListComponent implements OnInit{
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private pdfService = inject(PdfService);
+  private formBulder = inject(FormBuilder);
 
   user ?: boolean;
   memberships: Membership[] = [];
@@ -32,6 +34,7 @@ export default class MembershipsListComponent implements OnInit{
   searchText: string = '';
   hasActiveMembership: boolean = false;
   roles: any[] = [];
+  filterForm !: FormGroup;
 
   // Pagination
   page: number = 0;
@@ -39,10 +42,12 @@ export default class MembershipsListComponent implements OnInit{
   totalPages: number = 0;
 
   ngOnInit(): void {
-
     this.loginService.getRoles().subscribe((roles) => {
       this.roles = roles;
-    })
+    });
+
+    this.initFilterForm();
+    this.loadMembershipTypes();
 
     const userDocument = this.route.snapshot.paramMap.get('document');
 
@@ -69,13 +74,49 @@ export default class MembershipsListComponent implements OnInit{
     }
   }
 
+  initFilterForm() {
+    this.filterForm = this.formBulder.group({
+      userDocument: [''],
+      membershipType: [''],
+      membershipStatus: [''],
+      frozen: [''],
+      transferred: ['']
+    });
+
+    // Apply real time filters
+    this.filterForm.valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe(() => {
+        this.page = 0; // Reset page to 0 when filters change
+        this.loadMemberships();
+      });
+  }
+
+  resetFilters() {
+    this.page = 0;
+    this.filterForm.reset({
+      membershipType: { value: '', disabled: false },
+      membershipStatus: { value: '', disabled: false },
+    });
+  }
+
   loadMemberships() {
-    this.membershipsService.list(this.page, this.size)
+    const filters = this.filterForm.value;
+    this.membershipsService.list(this.page, this.size, filters)
     .subscribe((response) => {
       this.memberships = response.content;
       this.totalPages = response.totalPages;
       this.filteredMemberships = response.content;
     });
+  }
+
+  loadMembershipTypes() {
+    this.membershipsService.getMembershipTypes().subscribe((membershipTypes) => {
+      this.membershipTypes = membershipTypes;
+    })
   }
 
   isTransferable(membershipTypeName: string): boolean {
