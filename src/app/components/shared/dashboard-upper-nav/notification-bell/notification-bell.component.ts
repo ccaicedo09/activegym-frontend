2a653306ba3f1770a69e69c6703b3dd564614558
unknown <u20231213568@usco.edu.co>
2024-11-20 14:47:58 -0500

    add routing and fix notifications

--- src/app/components/shared/dashboard-upper-nav/notification-bell/notification-bell.component.ts
+++ src/app/components/shared/dashboard-upper-nav/notification-bell/notification-bell.component.ts
@@ -1,32 +1,33 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ExpiringNotification } from './ExpiringNotification.interface';
 import { AnalyticsService } from '../../../../services/analytics/analytics.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-notification-bell',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './notification-bell.component.html',
   styleUrl: './notification-bell.component.css'
 })
 export default class NotificationBellComponent implements OnInit {
   hasNotifications = false;
-  notifications: ExpiringNotification[] = [];
+  notifications: number = 0;
+  expiringMemberships: ExpiringNotification[] = [];
 
   private analyticsService = inject(AnalyticsService);
   private router = inject(Router);
 
   ngOnInit(): void {
-    this.analyticsService.hasActiveNotifications().subscribe(
-      has => this.hasNotifications = has
-    );
-
-    this.analyticsService.getNotifications().subscribe(
-      notifications => this.notifications = notifications
-    );
+    this.getExpiringMemberships();
   }
 
-  goToExpiringMemberships(): void {}
+  getExpiringMemberships(): void {
+    this.analyticsService.getExpiringMemberships().subscribe((notifications: ExpiringNotification[]) => {
+      this.expiringMemberships = notifications;
+      this.notifications = notifications.length;
+      this.hasNotifications = this.notifications > 0;
+    });
+  }
 }
